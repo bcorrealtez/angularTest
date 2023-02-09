e04e9a41f577ce00ef1e286e01c0893b08e1650c
bcorrea <brunocorreaaltez2016@gmail.com>
2023-02-09 10:48:43 -0300

    detail selected hero added

--- test1/src/app/components/toh-heroes/toh-heroes.component.ts
+++ test1/src/app/components/toh-heroes/toh-heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Hero } from "../toh-hero";
+import { HEROES } from "../toh-mock-heroes";
 
 @Component({
   selector: 'app-toh-heroes',
@@ -8,8 +9,9 @@ import { Hero } from "../toh-hero";
   styleUrls: ['./toh-heroes.component.css']
 })
 export class TohHeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
